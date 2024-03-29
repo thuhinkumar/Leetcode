6766302a1eb3b3207110f21f015916056d04cea5
thuhinkumar <51832407+thuhinkumar@users.noreply.github.com>
2024-03-29 01:41:50 -0400

    Runtime: 247 ms (56.64%), Memory: 93.4 MB (47.93%) - Gitcode

--- 435-non-overlapping-intervals/435. Non-overlapping Intervals.cpp
+++ 435-non-overlapping-intervals/435. Non-overlapping Intervals.cpp
@@ -5,14 +5,10 @@ public:
         int ans = 0;
         sort(intervals.begin(),intervals.end(),[](const vector<int> &v1, const vector<int> &v2){return v1[0] < v2[0];});
         //loop through every interval
-        int end = intervals[0][1];
         for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0] >= end){ //no overlap
-                end = intervals[i][1];
-            } 
-            else{ //overlaps
+            if(intervals[i-1][1] > intervals[i][0]){
+                intervals[i][1] = min(intervals[i][1],intervals[i-1][1]);
                 ans++;
-                end = min(intervals[i][1],end);
             }
         }
         //if there is a overlap remove the interval with the highest end

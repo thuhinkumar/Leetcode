56c4c9c9243fa80c9d81bec85a512f6b56b66430
thuhinkumar <51832407+thuhinkumar@users.noreply.github.com>
2024-05-07 18:11:24 -0400

    Runtime: 0 ms (100.00%), Memory: 7.3 MB (87.44%) - Gitcode

--- 7-reverse-integer/7. Reverse Integer.cpp
+++ 7-reverse-integer/7. Reverse Integer.cpp
@@ -2,11 +2,20 @@ class Solution {
 public:
     int reverse(int x) {
         int ans = 0;
-        while(x != 0 ){
-            if (ans>INT_MAX/10 || ans<INT_MIN/10) return 0;
+        while(x){
+            int num = x%10;
+
+            if (ans>INT_MAX/10||(ans == INT_MAX/10 && num> INT_MAX%10)){
+                return 0;
+            }
+
+            if (ans<INT_MIN/10||(ans == INT_MIN/10 && num < INT_MIN%10)){
+                return 0;
+            }
+
             ans *=10;
-            ans += x%10;
-            x = x/10;
+            ans += num;
+            x=x/10;
         }
         return ans;
     }
